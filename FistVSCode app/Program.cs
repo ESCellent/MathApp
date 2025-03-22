a1582af97dd5fd0c5a1d12979b7eae1d1e58a8de
Esc <vasercom7@gmail.com>
2025-03-22 23:35:22 +0300

    Added something

---
+++ FistVSCode app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SomePj 
+{
+        class Program
+        {
+            static void Main()
+            {
+                Console.WriteLine("Hello world!");
+                Console.WriteLine("Good evening!");
+            }
+        }
+}
